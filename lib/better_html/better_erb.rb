9a5375b0a68ec465301394439c0c050182eb30c1
EiNSTeiN_ <einstein@g3nius.org>
2016-12-28 21:50:38 +0100

    log template formats

--- lib/better_html/better_erb.rb
+++ lib/better_html/better_erb.rb
@@ -27,7 +27,7 @@ class BetterHtml::BetterErb
       # wrong, we can still find an encoding tag
       # (<%# encoding %>) inside the String using a regular
       # expression
-      puts "[GENERATE] #{template.virtual_path}"
+      puts "[GENERATE] #{template.virtual_path} #{template.formats.inspect}"
       template_source = template.source.dup.force_encoding(Encoding::ASCII_8BIT)
 
       erb = template_source.gsub(ActionView::Template::Handlers::ERB::ENCODING_TAG, '')
