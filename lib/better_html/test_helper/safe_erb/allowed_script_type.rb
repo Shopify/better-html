6e7529e96e0c8ce2e806c6833643b4221137377b
Dante Catalfamo <dante@Dantes-MacBook-Pro.local>
2022-06-10 16:53:58 -0400

    Revert "Allow module script type"
    This reverts commit 46f06f44fa53c815ed4cc80d6e154a5e41d8bd5e.

--- lib/better_html/test_helper/safe_erb/allowed_script_type.rb
+++ lib/better_html/test_helper/safe_erb/allowed_script_type.rb
@@ -4,7 +4,7 @@ module BetterHtml
   module TestHelper
     module SafeErb
       class AllowedScriptType < Base
-        VALID_JAVASCRIPT_TAG_TYPES = ['text/javascript', 'text/template', 'text/html', 'module']
+        VALID_JAVASCRIPT_TAG_TYPES = ['text/javascript', 'text/template', 'text/html']
 
         def validate
           script_tags.each do |tag, _|
