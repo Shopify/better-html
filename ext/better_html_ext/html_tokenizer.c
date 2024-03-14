e1a14db65f16f45f5a06fe3c24f4c191909a5b4c
Rafael Mendonça França <rafael.franca@shopify.com>
2024-03-14 22:44:14 +0000

    Avoid warning in the C extension

--- ext/better_html_ext/html_tokenizer.c
+++ ext/better_html_ext/html_tokenizer.c
@@ -4,7 +4,7 @@
 
 static VALUE mHtmlTokenizer = Qnil;
 
-void Init_better_html_ext()
+void Init_better_html_ext(void)
 {
   mHtmlTokenizer = rb_define_module("HtmlTokenizer");
   Init_html_tokenizer_tokenizer(mHtmlTokenizer);
