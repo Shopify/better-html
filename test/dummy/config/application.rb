e050e082f7bcfa6cbd6fc5da88eb57bd5ee5e40a
Étienne Barrié <etienne.barrie@shopify.com>
2024-03-19 11:08:18 +0100

    Make the dummy app bootable in test

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -21,7 +21,6 @@ module Dummy
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    config.action_view.annotate_rendered_view_with_filenames = true
   end
 end
